fcf5d2f79f55881295be84ecc86279e26a8f06e5
MJWcodr <matthias.wuensch175@gmail.com>
2021-01-28 21:37:17 +0100

    Add CPU and RAM limit

--- backup/cronjob/lbackup-hourly.sh
+++ backup/cronjob/lbackup-hourly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+systemd-run --scope -p CPUQuota=10% /usr/sbin/lbackup-hourly.sh
 restic -r "/srv/lbackups" --exclude-file "/srv/backup-exclude.txt" --password-file "/srv/backuppw.txt" backup --verbose $(cat /srv/backup.txt) 
 
 case $(rclone lsf onedrive:Personal/Backup) in 
