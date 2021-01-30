352b7da9c4142c12fafe46c8d563484cce6a7311
MJWcodr <matthias.wuensch175@gmail.com>
2021-01-30 15:37:22 +0100

    improve shell output

--- install.sh
+++ install.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
+# check for sudo rights
 if [ $(whoami) != root ]; then
-	echo "Please run as root, exiting"
+	echo -e "Please run as root, exiting"
 	exit
 fi
+echo -e "installation start"
+# define what text is bold
+bold=$(tput bold)
+normal=$(tput sgr0)
 
+# update all repositories
 apt -qq update
 
-echo "installation start"
-printf "\n Do you want to run installprogs.sh to install some applications? (y/n)"
+
+
+echo -e "\fDo you want to run installprogs.sh to install some applications? (y/n)"
 read -r yesno
 if [ "$yesno" = "y" ]; then
 ./installprogs.sh
 fi
 
-echo "\n Do you want to run the rclone configuration script? (y/n)"
+echo -e "\fDo you want to run the rclone configuration script? (y/n)"
 read -r yesno
 if [ "$yesno" = "y" ]; then
 ./rclone/rclone.sh
 fi
 
-printf "\n Do you want to run the backup configuration script? (y/n)"
+echo -e "\fDo you want to run the backup configuration script? (y/n)"
 read -r yesno
 if [ "$yesno" = "y" ]; then
 ./backup/backup.sh
 fi
 
-printf "\n Do you want to run the security script? (y/n)"
+echo -e "\fDo you want to run the security script? (y/n)"
 read -r yesno
 if [ "$yesno" = "y" ]; then
 	./security/security.sh
 fi
-echo "finished"
+echo -e "finished"
