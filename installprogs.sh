05c62f4266d5c6fa444c7e858c791605713fa85e
MJWcodr <matthias.wuensch175@gmail.com>
2021-01-30 15:40:42 +0100

    improve shell output

--- installprogs.sh
+++ installprogs.sh
@@ -1,8 +1,8 @@
-add-apt-repository ppa:phoerious/keepassxc -y
-add-apt-repository ppa:unit193/encryption -y
+add-apt-repository -qq  ppa:phoerious/keepassxc -y
+add-apt-repository -qq ppa:unit193/encryption -y
 
-apt update
-apt install keepassxc rclone git veracrypt snap gpg -y
+apt -qq update
+apt -qq install keepassxc rclone git veracrypt snap gpg -y
 
 snap install --classic code # or code-insider
 snap install spotify
