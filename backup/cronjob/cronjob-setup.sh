1b40ce4cbe38fdb9a983376bdd55f3365cf6d742
MJWcodr <matthias.wuensch175@gmail.com>
2021-02-01 15:06:51 +0100

    make lbackup-hourly compliant with debian guidelines

--- backup/cronjob/cronjob-setup.sh
+++ backup/cronjob/cronjob-setup.sh
@@ -2,17 +2,17 @@
 cd "./cronjob" || echo " \e[31mchanging directory into ./cronjob-setup failed"
 # copy the backup-hourly.sh script into the proper directory
 
-if [ -f "/etc/cron.hourly/lbackup-hourly.sh" ]; then
-	echo "lbackup-hourly.sh is already a cronjob"
+if [ -f "/etc/cron.hourly/lbackup-hourly" ]; then
+	echo "lbackup-hourly  is already a cronjob"
 	echo -e "\fdo you want to update it?"
 	read -r yesno
 	if [ $yesno = "yes" ]; then
-		cp -f ./lbackup-hourly.sh /etc/cron.hourly/lbackup-hourly.sh
+		cp -f ./lbackup-hourly  /etc/cron.hourly/lbackup-hourly
 	else
-		/etc/cron.hourly/lbackup-hourly.sh
+		/etc/cron.hourly/lbackup-hourly
 	fi
 else
-	cp -f ./lbackup-hourly.sh /etc/cron.hourly/lbackup-hourly.sh
+	cp -f ./lbackup-hourly  /etc/cron.hourly/lbackup-hourly
 fi
 
 cd ..
