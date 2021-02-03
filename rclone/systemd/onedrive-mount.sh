c56912981e5238e94ab12ed6d035e7275f88a6c8
MJWcodr <matthias.wuensch175@gmail.com>
2021-02-04 00:52:31 +0100

    fix vfs-cache (again)

--- rclone/systemd/onedrive-mount.sh
+++ rclone/systemd/onedrive-mount.sh
@@ -1,5 +1,5 @@
 if [ -d  /home/matthias/Onedrive ]; then
-	rclone mount --allow-other --vfs-cache-mode onedrive: /home/matthias/Onedrive
+	rclone mount --allow-other --vfs-cache-mode full onedrive: /home/matthias/Onedrive
 else
 	mkdir /home/matthias/Onedrive
 	rclone mount --allow-other --vfs-cache-mode full onedrive: /home/matthias/Onedrive
